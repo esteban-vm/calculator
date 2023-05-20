ab449af2f77766a8e6ce42a60cd8ea9d097980dc
esteban-vm <estebanvm90@outlook.com>
2023-05-19 22:19:29 -0400

    refactors

--- src/hooks/useCalculator/useCalculator.test.ts
+++ src/hooks/useCalculator/useCalculator.test.ts
@@ -4,18 +4,20 @@ import useCalculator from './useCalculator'
 describe('USE CALCULATOR TEST SUITE', () => {
   it('should return the correct initial values', () => {
     const { result } = renderHook(useCalculator)
-    expect(result.current.current).toBe('')
-    expect(result.current.previous).toBe('')
-    expect(result.current.operation).toBe('')
+    const { current, previous, operation } = result.current
+    expect(current).toBe('')
+    expect(previous).toBe('')
+    expect(operation).toBe('')
   })
 
   it('should return the functions to update those values', () => {
     const { result } = renderHook(useCalculator)
-    expect(typeof result.current.appendValue).toBe('function')
-    expect(typeof result.current.clearEntry).toBe('function')
-    expect(typeof result.current.clearAll).toBe('function')
-    expect(typeof result.current.chooseOperation).toBe('function')
-    expect(typeof result.current.getResult).toBe('function')
-    expect(typeof result.current.toggleSign).toBe('function')
+    const { appendValue, clearEntry, clearAll, chooseOperation, getResult, toggleSign } = result.current
+    expect(typeof appendValue).toBe('function')
+    expect(typeof clearEntry).toBe('function')
+    expect(typeof clearAll).toBe('function')
+    expect(typeof chooseOperation).toBe('function')
+    expect(typeof getResult).toBe('function')
+    expect(typeof toggleSign).toBe('function')
   })
 })
