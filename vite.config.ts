4f1df7f4241de9e1111340a8fc7ec587ed6f6781
esteban-vm <estebanvm90@outlook.com>
2023-05-22 16:05:14 -0400

    adding pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      manifest: {
+        name: 'Simple Calculator',
+        short_name: 'Simple Calculator App',
+        description: 'App made with React JS, Vite and TypeScript',
+        display: 'fullscreen',
+        orientation: 'any',
+        start_url: '/',
+        theme_color: '#ff3f7b',
+        icons: [
+          {
+            src: '/icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      includeAssets: ['/css/*.css'],
+      registerType: 'autoUpdate',
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
